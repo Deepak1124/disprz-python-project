bce88f54c4ff135c9d9417d2774104db0b790f49
Deepak Kumar <deepak.kumar@joshsoftware.com>
2023-05-31 13:11:07 +0530

    done with 6th module assignment

--- module-6/getUniqueNos.py
+++ module-6/getUniqueNos.py
@@ -1,10 +1,5 @@
 # [1,2,3,1,3,4,6,5,3] get unique nums from list with basic constructs
 
-nums = [1, 2, 3, 1, 3, 4, 6, 5, 3]
-unique_nums = []
-
-for num in nums:
-    if num not in unique_nums:
-        unique_nums.append(num)
-
-print(unique_nums)
+numbers = [1, 2, 3, 1, 3, 4, 6, 5, 3]
+unique_numbers = set(numbers)
+print(unique_numbers)
--- module-6/replaceDigitsfromListOfStrings.py
+++ module-6/replaceDigitsfromListOfStrings.py
@@ -1,15 +1,9 @@
 
 # [“name512”, “same1example”, “joy18full”] replace the digits from string inside list
 
+
+import re
 str_list = ["name512", "same1example", "joy18full"]
+new_str_list = [re.sub('\d+', '', str) for str in str_list]
 
-updated_str_list = []
-
-for str in str_list:
-    updated_str = ""
-    for char in str:
-        if not char.isdigit():
-            updated_str += char
-    updated_str_list.append(updated_str)
-
-print(updated_str_list)
+print(new_str_list)
